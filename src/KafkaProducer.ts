9407007bd7ee1eb5ee74c6c8636ab06ed868203c
Harshith Gowda <harshithgowdakt@gmail.com>
2022-02-06 23:24:54 +0530

    added kafka producer example

---
+++ src/KafkaProducer.ts
@@ -0,0 +1,56 @@
+import { Kafka, Producer } from "kafkajs";
+
+class KafkaAdmin {
+    private kafka: Kafka;
+    private producer: Producer;
+
+    constructor() {
+        this.kafka = new Kafka({
+            clientId: "kafka-nodejs",
+            brokers: ["localhost:29092"]
+        });
+        this.producer = this.kafka.producer();
+    }
+
+    get Producer(): Producer {
+        if (!this.producer) {
+            this.producer = this.kafka.producer();
+        }
+        return this.producer;
+    }
+
+    async send() {
+        try {
+            await this.Producer.connect();
+            await this.Producer.send({
+                topic: "learning",
+                messages: [{ key: "name", value: "Harshith" }, { key: "role", value: "SDE" }]
+            });
+            console.log("sent message successfully");
+            await this.Producer.disconnect();
+        } catch (error) {
+            console.log("error while sending message", error);
+        }
+    }
+
+    async sendBatch() {
+        await this.Producer.connect();
+        let topics = await this.Producer.sendBatch({
+            topicMessages: [{
+                topic: "learning",
+                messages: [{ key: "name", value: "Gowda" }, { key: "role", value: "SDE" }]
+            }, {
+                topic: "learning",
+                messages: [{ key: "name", value: "Gowda" }, { key: "role", value: "SDE" }]
+            }]
+        });
+        await this.Producer.disconnect();
+        return topics;
+    }
+}
+
+(async function () {
+    let client = new KafkaAdmin();
+    await client.send();
+    await client.sendBatch();
+})()
